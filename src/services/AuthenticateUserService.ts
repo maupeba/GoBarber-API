b60b7359a7b0f9cb3ad6298b8a2b197d57dbe784
Mauricio de Sousa <mauriciopeba1234@gmail.com>
2020-06-16 16:43:21 -0300

    tratativa de erros

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,8 +1,10 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-
 import authConfig from '../config/auth';
+
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -22,13 +24,13 @@ class AuthenticateUserService {
     const user = await userRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect email/passord combination.');
+      throw new AppError('Incorrect email/passord combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/passord combination.');
+      throw new AppError('Incorrect email/passord combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,8 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -18,7 +20,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw Error('Email address already exists');
+      throw AppError('Email address already exists');
     }
 
     const hashedPassword = await hash(password, 8);
